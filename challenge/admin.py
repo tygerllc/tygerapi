a5084bc2a0751411385cb627113477f84446a932
Tim Ogilvie <me@timogilvie.com>
2011-11-01 08:11:58 -0400

    Added disqus commenting to challenge_detail

--- challenge/admin.py
+++ challenge/admin.py
@@ -1,4 +1,4 @@
-from challenge.models import Challenge, Criteria, UserProfile
+from challenge.models import Challenge, Criteria, UserProfile, Useful_Component, Useful_Link
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
@@ -16,6 +16,14 @@ class CriteriaInline(admin.TabularInline):
     model = Criteria
     extra = 2
 
+class ComponentInline(admin.TabularInline):
+    model = Useful_Component
+    extra = 2
+
+class RefInline(admin.TabularInline):
+    model = Useful_Link
+    extra = 2
+    
 class ChallengeAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'votes', 'bounty', 'bounty_avail')
     prepopulated_fields = {'slug': ('name',)}
@@ -23,7 +31,7 @@ class ChallengeAdmin(admin.ModelAdmin):
         ('General', {'fields': ['name', 'slug', 'tags', 'descrip', 'sponsor']}),
         ('Rewards', {'fields': ['votes', 'bounty']})
     ]
-    inlines = [CriteriaInline]
+    inlines = [CriteriaInline, ComponentInline, RefInline]
     search_fields = ['name']
 
 admin.site.register(User, UserProfileAdmin)
