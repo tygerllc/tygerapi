11c61cba80ffc4ffc7e523a92f18bd4a695a613d
unknown <Jugal@Burrow.(none)>
2011-11-10 00:37:53 +0530

    Updated Styles

--- urls.py
+++ urls.py
@@ -1,11 +1,10 @@
-# This also imports the include function
 from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^challenge/', include('challenge.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+	(r'^challenge/', include('challenge.urls')),
+	(r'^admin/', include(admin.site.urls)),
 )
 
