355f375b12c695493f3d0e5396efe981da5e86de
Jugal Manjeshwar <jugalm9@gmail.com>
2011-12-17 08:53:19 +0530

    Update Challenge Tag listing to display currently selected tag. 1. Add Sidebar block to 'challenge_with_tag'. 2. Create template tag 'active_tag' to detect selected tag and highlight accordingly. 3. Optimize current tab check code in challenge listing.

--- misc/templatetags/tags.py
+++ misc/templatetags/tags.py
@@ -1,9 +1,16 @@
 from django import template
+import re
 
 register = template.Library()
 @register.simple_tag
 def current(request, pattern):
-    import re
     if re.search(pattern, request.path):
         return 'current'
     return ''
+
+@register.simple_tag
+def active_tag(request, tag_name):
+	pattern = '/'+tag_name+'/?$'
+	if re.search(pattern, request.path):
+		return 'active'
+	return ''
