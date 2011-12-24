a14fbd60e87441606b872cad4d862e43e982d476
Tim Ogilvie <me@timogilvie.com>
2011-12-24 13:02:30 -0500

    Added API and simple handler for bench

--- api/handlers.py
+++ api/handlers.py
@@ -1,9 +1,27 @@
 from piston.handler import BaseHandler
 from bench.models import Bench
+from challenge.models import Challenge, Criteria, Environment, SourceSink
+
+class SourceSinkHandler(BaseHandler):
+    model = SourceSink
+
+class EnvironmentHandler(BaseHandler):
+    model = Environment
+    fields = ('id', 'name', 'temp', 'pH', ('sources_and_sinks', ()))
+
+class ChallengeHandler(BaseHandler):
+      model = Challenge
+      fields = ('id', 'name', 'tags', 'descrip', 'votes', 'bounty', 'chassis',
+                ('environment', ()),
+                )
 
 class BenchHandler( BaseHandler ):
     allowed_methods = ('GET',)
     model = Bench
+    fields = ('id', 'name', 'desc', 'slug',
+              ('challenge', ()),
+              ('author', ('id', 'username')),
+        )
 
     def read(self, request, slug=None):
         base = Bench.objects
